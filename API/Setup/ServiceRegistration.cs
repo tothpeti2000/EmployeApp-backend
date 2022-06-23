f726fc806d406ec4a45ed0b78d0ea91aad0f449a
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-06-23 17:15:20 +0200

    Service registration separated into multiple methods

--- API/Setup/ServiceRegistration.cs
+++ API/Setup/ServiceRegistration.cs
@@ -6,12 +6,24 @@ namespace API.Setup
 {
     public static class ServiceRegistration
     {
+        // Add services to the container.
         public static WebApplicationBuilder RegisterServices(this WebApplicationBuilder builder)
         {
-            // Add services to the container.
-            builder.Services.AddControllers();
+            RegisterControllers(builder);
+            RegisterDbContext(builder);
+            RegisterSwagger(builder);
+            RegisterCustomServices(builder);
 
-            // DB connection
+            return builder;
+        }
+
+        private static void RegisterControllers(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllers();
+        }
+
+        private static void RegisterDbContext(this WebApplicationBuilder builder)
+        {
             var connStr = builder.Configuration.GetConnectionString("ConnStr");
 
             if (connStr == null)
@@ -20,15 +32,19 @@ namespace API.Setup
             }
 
             builder.Services.AddDbContext<EmployeeContext>(options => options.UseSqlServer(connStr));
+        }
 
+        private static void RegisterSwagger(this WebApplicationBuilder builder)
+        {
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+        }
 
+        private static void RegisterCustomServices(this WebApplicationBuilder builder)
+        {
             builder.Services.AddScoped<EmployeeService, EmployeeService>();
             builder.Services.AddScoped<DepartmentService, DepartmentService>();
-
-            return builder;
         }
     }
 }
